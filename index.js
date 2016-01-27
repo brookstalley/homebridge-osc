5f86fdbb96c3f20d31d4315bbe9738bc2b587eb2
Brooks Talley <brooks@tangentry.com>
2016-01-26 20:53:31 -0800

    Many revs to get OSC going

--- index.js
+++ index.js
@@ -1,101 +1,65 @@
+var osc = require("osc");
+
 var Service, Characteristic;
 
-var midi = require("midi");
-var midiOutputs = [];
-var midiInputs = [];
-var midiWatches = [];
+var getIPAddresses = function () {
+    var os = require("os"),
+        interfaces = os.networkInterfaces(),
+        ipAddresses = [];
 
-// Midi -- return port number or throw error if not found
-function OutPortNumberFromName(portName) {
-    var out = new midi.output();
-    for (var i = 0; i < out.getPortCount() ; i++) {
-        if (out.getPortName(i) == portName) {
-            //console.log("Got output #" + i + " for port " + portName);
-            return i;
+    for (var deviceName in interfaces) {
+        var addresses = interfaces[deviceName];
+        for (var i = 0; i < addresses.length; i++) {
+            var addressInfo = addresses[i];
+            if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+                ipAddresses.push(addressInfo.address);
+            }
         }
     }
-    throw new Error("Couldn't find MIDI port '" + portName + "'");
-}
 
-function OutputForPortNumber(portNumber) {
-    if (typeof midiOutputs[portNumber] == 'undefined') {
-        midiOutputs[portNumber] = new midi.output();
-        midiOutputs[portNumber].openPort(portNumber);
-        //console.log("Adding output #" + portNumber);
-    }
-    return midiOutputs[portNumber];
-}
+    return ipAddresses;
+};
 
-// Midi -- return port number or throw error if not found
-function InPortNumberFromName(portName) {
-    var inp = new midi.input();
-    for (var i = 0; i < inp.getPortCount() ; i++) {
-        console.log('Checking port ' + i + '(' + inp.getPortName(i) + ')');
-        if (inp.getPortName(i) == portName) {
-            console.log("Got output #" + i + " for port " + portName);
-            return i;
-        }
-    }
-    throw new Error("Couldn't find MIDI port '" + portName + "'");
-}
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 8000
+});
 
-function InputForPortNumber(portNumber) {
-    if (typeof midiInputs[portNumber] == 'undefined') {
-        console.log("Adding output #" + portNumber);
-        midiInputs[portNumber] = new midi.input();
-        midiInputs[portNumber].on('message', function (deltaTime, message) {
-            MidiInput(portNumber, deltaTime, message);
-        });
-        midiInputs[portNumber].openPort(portNumber);
+udpPort.on("ready", function () {
+    var ipAddresses = getIPAddresses();
 
-    }
-    return midiInputs[portNumber];
-}
+    console.log("Listening for OSC over UDP.");
+    ipAddresses.forEach(function (address) {
+        console.log(" OSC listening on host:", address + ":", udpPort.options.localPort);
+    });
+});
+
+// Listen for incoming OSC bundles. 
+udpPort.on("bundle", function (oscBundle) {
+    console.log("An OSC bundle just arrived!", oscBundle);
+});
+
+// Open the socket. 
+udpPort.open();
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-midi", "Midi", MidiAccessory);
+    homebridge.registerAccessory("homebridge-osc", "OSC", OscAccessory);
 }
 
-function MidiInput(portNumber, deltaTime, message) {
-    // The message is an array of numbers corresponding to the MIDI bytes:
-    //   [status, data1, data2]
-    // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
-    // information interpreting the messages.
-    //console.log('p: ' + port + ', m:' + message + ' d:' + deltaTime);
-    messageType = message[0];
-    controller = message[1];
-    value = message[2];
-    //console.log("Got input for port " + portNumber + ", message " + messageType);
-    if (messageType >= 176 && messageType < 192) {
-        channel = messageType - 175;
-        controller = controller + 1;
-        //console.log("Got input for port " + portNumber + ", channel " + channel + ", controller " + controller + " (" + value + ")");
-        midiWatches.forEach(function (watch) {
-
-            if (portNumber == watch.portNumber && channel == watch.channel && controller == watch.controller) {
-                //console.log("Triggered watch for port " + portNumber + ", channel " + channel + ", controller " + controller + " (" + value + ")");
-                watch.callback(value);
-            } else {
-                //console.log("No match for watch port " + watch.portNumber + ", channel " + watch.channel + ", controller " + watch.controller);
-            }
-        })
-    }
-
-}
-
-function MidiAccessory(log, config) {
+function OscAccessory(log, config) {
     this.log = log;
 
-    this.outPortNumber = OutPortNumberFromName(config["port"]);
-    this.channel = parseInt(config["channel"] || 1);
-    this.note = config["note"];
-    this.log("  port " + this.outPortNumber + ", channel " + this.channel + ", note " + this.note);
+    this.remoteAddress = config["remoteAddress"] || "255.255.255.255"; // Not really inet broadcast, will detect and use local subnet broadcast in send routine
+    this.remotePort = parseInt(config["port"] || 9001);
     this.service = config["service"] || "Switch";
     this.name = config["name"];
+    this.messages = config["messages"];
+    this.log("OSC Accessory '" + this.name + "' on " + this.remoteAddress + ":" + this.remotePort + this.address + " ID " + this.id);
+
     this.brightness = 0;
-    if (characteristics = config["characteristics"] ) {
+    if (characteristics = config["characteristics"]) {
         characteristics.forEach(function (characteristic) {
             var characteristicPort = characteristic["port"];
             var characteristicType = characteristic["characteristic"];
@@ -117,24 +81,28 @@ function MidiAccessory(log, config) {
                 }.bind(this)
             };
             midiWatches.push(newWatch);
-    
+
         }.bind(this))
     }
 }
 
-MidiAccessory.prototype = {
+OscAccessory.prototype = {
 
-    midiRequest: function (outPortNumber, action, channel, note, velocity, callback) {
-        if (action == 'noteon') {
-            byteOne = 144 + (channel - 1);
-        } else if (action == 'noteoff') {
-            byteOne = 128 + (channel - 1);
-        } else {
-            throw new Error("Unrecognized MIDI action");
-        }
-        var bytes = [byteOne, note, velocity];
-        OutputForPortNumber(outPortNumber).sendMessage(bytes);
-        this.log("Sent MIDI message " + bytes);
+    oscRequest: function (remoteAddress, remotePort, messages, callback) {
+        // Send an OSC message to, say, SuperCollider 
+
+        var b = (remoteAddress == "255.255.255.255") ? true : false;
+
+        Object.keys(this.messages).forEach(function (address) {
+            console.log("Sending to " + address + " on " + remoteAddress + ":" + remotePort + " (broadcast: " + b + ")");
+            udpPort.send({
+                address: address,
+                //broadcast: b,
+                args: messages[address]
+            }, remoteAddress, remotePort);
+            console.log("Sent OSC message");
+        })
+
         callback();
     },
 
@@ -143,35 +111,28 @@ MidiAccessory.prototype = {
         var action;
 
         if (State) {
-            action = 'noteon';
-            velocity = 127;
-            // Make it turn back off in a second since we're just emitting the event
-            setTimeout(function () {
-                this._service.setCharacteristic(Characteristic.On, false);
-            }.bind(this), 1000);
-
+            value = 1.0;
         } else {
-            action = 'noteoff';
-            velocity = 0;
+            value = 0.0;
         }
-        this.midiRequest(this.outPortNumber, action, this.channel, this.note, velocity, function (error, response) {
+        this.oscRequest(this.remoteAddress, this.remotePort, this.messages, function (error, response) {
             if (error) {
-                this.log('Midi function failed: %s', error.message);
+                this.log('OSC function failed: %s', error.message);
                 callback(error);
             } else {
-                this.log('Midi function succeeded');
+                this.log('OSC function succeeded');
                 callback();
             }
         }.bind(this));
     },
 
-    getBrightness: function(callback) {
+    getBrightness: function (callback) {
         this.log("Brightness requested!");
         console.log("Current brightness: " + this.brightness);
         callback(null, this.brightness);
     },
 
-    setBrightness: function(value, callback) {
+    setBrightness: function (value, callback) {
         this.log("Setting brightness to " + value);
         this._service.setCharacteristic(Characteristic.Brightness, value);
         callback();
@@ -189,9 +150,9 @@ MidiAccessory.prototype = {
         var informationService = new Service.AccessoryInformation();
 
         informationService
-			.setCharacteristic(Characteristic.Manufacturer, "Midi Manufacturer")
-			.setCharacteristic(Characteristic.Model, "Midi Model")
-			.setCharacteristic(Characteristic.SerialNumber, "Midi Serial Number");
+			.setCharacteristic(Characteristic.Manufacturer, "OSC Manufacturer")
+			.setCharacteristic(Characteristic.Model, "OSC Model")
+			.setCharacteristic(Characteristic.SerialNumber, "OSC Serial Number");
 
         if (this.service == "Switch") {
             this._service = new Service.Switch(this.name);
@@ -199,7 +160,7 @@ MidiAccessory.prototype = {
             this._service
 				.getCharacteristic(Characteristic.On)
 				.on('set', this.setPowerState.bind(this));
-            
+
             this._service
                 .addCharacteristic(Characteristic.Brightness)
                 .on('set', this.setBrightness.bind(this))
