6cd9a04406afc5cd3c2ed56f15330c359a79b5bf
Brooks Talley <brooks@tangentry.com>
2016-01-30 15:40:34 -0800

    Moved to multiswitch radiobutton model; many fixes

--- README.md
+++ README.md
@@ -9,36 +9,44 @@ This plugin sends and receives OSC commands and may be useful for getting Homeki
 2. Install node osc package: npm install homebridge-osc (note that on Raspberry Pi I had to run 'sudo npm install -g --unsafe-perm')
 3. Update your configuration file. See the sample below.
 
+*** Raspberry Pi notes:
+1. If errors with serialport, try sudo npm install -g --unsafe-perm serialport
+
+
 # Configuration
+
+At present, this plugin only supports the "multiswitch" service type, which groups a number of virtual switches in radio-button style (turn one on, the others turn off). This is designed to switch scenes on a lighting controller, but can probably be used for other purposes. 
+
+The "address" field is the OSC address to be sent and received on. For the multiswitch type, each of the switches' state is sent as an argument to that address, and any OSC messages recevied on that address are used to set Homekit switch statuses. This enables bidirectional Homekit /  OSC integration with all systems having the correct current state.
+
+In the below example, using a Homekit application to turn the "Daytime" switch on will send the OSC message "/0/grid-switch/x 0 0 100 0 0 0 0 0" via UDP to 10.0.0.9:9001
+
+If the message "/0/grid-switch/x 0 0 0 0 0 0 100 0" is received on any local IP address, port 8000, Homekit state will be updated to reflect that the "Bedtime" switch is on and all others are off.
 
 Configuration sample:
 
  ```
 {
-   "accessories": [
+  "accessories": [
     {
       "accessory": "OSC",
-      "adapter": "127.0.0.1",
-      "port": 9001,
-      "id": 32,
-      "name": "grid-sleep",
-      "service": "Switch"
-    },
-    {
-      "accessory": "OSC",
-      "adapter": "127.0.0.1",
-      "port": 9001,
-      "id": 34,
-      "name": "grid-daylight",
-      "service": "Switch"
-    },
-    {
-      "accessory": "OSC",
-      "adapter": "127.0.0.1",
-      "port": 9001,
-      "id":  35,
-      "name": "grid-evening",
-      "service": "Switch"
+      "name": "Grid Select",
+      "localAddress": "0.0.0.0",
+      "localPort":  8000,
+      "remoteAddress": "10.0.0.9",
+      "remotePort": 9001,
+      "address": "/0/grid-switch/x",
+      "service": "Multiswitch",
+      "switches": [
+        "Sleep",
+        "Wake",
+        "Daytime",
+        "Evening",
+        "Evening2",
+        "Movie",
+        "Bedtime",
+        "Blacklight"
+      ]
     }
   ]
 }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var osc = require("osc");
 
 var Service, Characteristic;
 
+var udpPorts = [];
+
 var getIPAddresses = function () {
     var os = require("os"),
         interfaces = os.networkInterfaces(),
@@ -20,27 +22,81 @@ var getIPAddresses = function () {
     return ipAddresses;
 };
 
-var udpPort = new osc.UDPPort({
-    localAddress: "0.0.0.0",
-    localPort: 8000
-});
-
-udpPort.on("ready", function () {
-    var ipAddresses = getIPAddresses();
-
-    console.log("Listening for OSC over UDP.");
-    ipAddresses.forEach(function (address) {
-        console.log(" OSC listening on host:", address + ":", udpPort.options.localPort);
+function getUdpPort(localAddress, localPort) {
+    udpPorts.forEach(function (port) {
+        if (port.localAddress == localAddress && port.localPort == localPort) {
+            return port;
+        }
     });
-});
 
-// Listen for incoming OSC bundles. 
-udpPort.on("bundle", function (oscBundle) {
-    console.log("An OSC bundle just arrived!", oscBundle);
-});
+    var newPort = new osc.UDPPort({
+        localAddress: localAddress,
+        localPort: localPort
+    });
 
-// Open the socket. 
-udpPort.open();
+    newPort.on("ready", function () {
+        var ipAddresses = getIPAddresses();
+
+        ipAddresses.forEach(function (ipAddress) {
+            console.log(" OSC listening on host:", ipAddress + ":", localPort);
+        });
+    });
+
+    // Listen for incoming OSC bundles. 
+    newPort.on("bundle", function (oscBundle) {
+        var packets = oscBundle["packets"];
+
+        packets.forEach(function (packet) {
+            var address = packet["address"];
+            var args = packet["args"];
+
+            if (address == this.address) {
+                this.log("OSC packet for " + this.name + " with args " + args.join(","));
+
+                // If we're a multiswitch, need to configure the current values of each switch
+                for (var i = 0; i < args.length; i++) {
+                    if (parseInt(args[i]) > 0) {
+                        newValue = true;
+                    } else {
+                        newValue = false;
+                    }
+                    this.services[i].getCharacteristic(Characteristic.On).setValue(newValue, undefined, "fromSetPowerState");
+                }
+            }
+        }.bind(this));
+    }.bind(this));
+
+    // Open the socket. 
+    newPort.open();
+
+    udpPorts.push(newPort);
+    return newPort;
+}
+
+//debugging helper only
+//inspects an object and prints its properties (also inherited properties) 
+var iterate = function nextIteration(myObject, path) {
+    // this function iterates over all properties of an object and print them to the console
+    // when finding objects it goes one level  deeper
+    var name;
+    if (!path) {
+        console.log("---iterating--------------------")
+    }
+    for (name in myObject) {
+        if (typeof myObject[name] !== 'function') {
+            if (typeof myObject[name] !== 'object') {
+                console.log((path || "") + name + ': ' + myObject[name]);
+            } else {
+                nextIteration(myObject[name], path ? path + name + "." : name + ".");
+            }
+        } else {
+            console.log((path || "") + name + ': (function)');
+        }
+    }
+    if (!path) {
+        console.log("================================");
+    }
+};
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
@@ -51,84 +107,99 @@ module.exports = function (homebridge) {
 function OscAccessory(log, config) {
     this.log = log;
 
+    this.name = config["name"];
+    this.accessoryType = config["service"] || "Switch";
+
     this.remoteAddress = config["remoteAddress"] || "255.255.255.255"; // Not really inet broadcast, will detect and use local subnet broadcast in send routine
     this.remotePort = parseInt(config["port"] || 9001);
-    this.service = config["service"] || "Switch";
-    this.name = config["name"];
-    this.messages = config["messages"];
-    this.log("OSC Accessory '" + this.name + "' on " + this.remoteAddress + ":" + this.remotePort + this.address + " ID " + this.id);
+    this.localAddress = config["localAddress"] || "0.0.0.0";
+    this.localPort = config["localPort"] || 9001;
+    this.address = config["address"];
 
-    this.brightness = 0;
-    if (characteristics = config["characteristics"]) {
-        characteristics.forEach(function (characteristic) {
-            var characteristicPort = characteristic["port"];
-            var characteristicType = characteristic["characteristic"];
-            var port = characteristic["port"];
-            var channel = characteristic["channel"];
-            var controller = characteristic["controller"];
-            var inPortNumber = InPortNumberFromName(port);
-            var inPort = InputForPortNumber(inPortNumber);
-            console.log('Adding MIDI input monitor for port ' + inPortNumber + ', channel ' + channel + ', controller ' + controller);
-            var newWatch = {
-                portNumber: inPortNumber, channel: channel, controller: controller, callback: function (value) {
-                    //console.log("Got a value!");
-                    if (value != this.brightness) {
-                        //console.log("*** Setting brightness to " + value);
+    myGetUdpPort = getUdpPort.bind(this);
+    this.udpPort = myGetUdpPort(this.localAddress, this.localPort);
 
-                        this.brightness = (value <= 100 ? value : 100);
-                        console.log("*** Brightness for " + this.name + " now " + this.brightness)
-                    }
-                }.bind(this)
-            };
-            midiWatches.push(newWatch);
-
-        }.bind(this))
+    switch (this.accessoryType) {
+        case "Multiswitch":
+            this.switches = config["switches"];
+            break;
+        case "Switch":
+            break;
+        default:
+            throw new Error("Unknown homebridge-osc accessory type");
     }
+
+    this.log("OSC Accessory " + this.name + "(" + this.accessoryType + ") on " + this.remoteAddress + ":" + this.remotePort + this.address);
 }
 
 OscAccessory.prototype = {
 
-    oscRequest: function (remoteAddress, remotePort, messages, callback) {
-        // Send an OSC message to, say, SuperCollider 
-
-        var b = (remoteAddress == "255.255.255.255") ? true : false;
-
-        Object.keys(this.messages).forEach(function (address) {
-            console.log("Sending to " + address + " on " + remoteAddress + ":" + remotePort + " (broadcast: " + b + ")");
-            udpPort.send({
-                address: address,
-                //broadcast: b,
-                args: messages[address]
-            }, remoteAddress, remotePort);
-            console.log("Sent OSC message");
-        })
-
-        callback();
-    },
-
-    setPowerState: function (State, callback) {
-        var velocity;
-        var action;
-
-        if (State) {
-            value = 1.0;
-        } else {
-            value = 0.0;
-        }
-        this.oscRequest(this.remoteAddress, this.remotePort, this.messages, function (error, response) {
-            if (error) {
-                this.log('OSC function failed: %s', error.message);
-                callback(error);
-            } else {
-                this.log('OSC function succeeded');
+    setPowerState: function (targetService, state, callback, context) {
+        self = this;
+        var myContext = "fromSetPowerState";
+        
+        if (context == myContext) {
+            // It came from us, so don't get stuck in a loop
+            if (callback) {
                 callback();
             }
-        }.bind(this));
+            return;
+        }
+        if (state) {
+            value = 100;
+        } else {
+            value = 0;
+        }
+
+        switch (this.accessoryType) {
+            case "Multiswitch":
+                var args = [];
+
+                this.services.forEach(function (service) {
+                    // If this is our switch, add 1 to the OSC output, otherwise add 0 and set to off, supplying the context
+                    // so it doesn't trigger an OSC message (we'll send 0 here) or endless loop (which is bad form).
+                    if (targetService.subtype == service.subtype) {
+                        args.push(1);
+                    } else {
+                        args.push(0);
+                        service.getCharacteristic(Characteristic.On).setValue(false, undefined, myContext);
+                    }
+                }
+                );
+                var broadcast = (this.remoteAddress == "255.255.255.255") ? true : false;
+                //this.log("Sending to " + this.address + " on " + this.remoteAddress + ":" + this.remotePort + " (broadcast: " + broadcast + "):");
+
+                this.udpPort.send({
+                    address: this.address,
+                    //broadcast: broadcast,
+                    args: args
+                }, this.remoteAddress, this.remotePort);
+
+                callback();
+                break;
+
+            case "Switch":
+                // Obsolete code from single-switch accessory sending multiple messages. Might still come in handy. So here it is.
+                this.oscRequest(this.messages, function (error, response) {
+                    if (error) {
+                        this.log('OSC function failed: %s', error.message);
+                        callback(error);
+                    } else {
+                        this.log('OSC function succeeded');
+                        callback();
+                    }
+                }.bind(this));
+                callback();
+                break;
+            default:
+                throw new Error("Unknown homebrdige-osc accessory type in setPowerState");
+        }
+
     },
 
     getBrightness: function (callback) {
         this.log("Brightness requested!");
-        console.log("Current brightness: " + this.brightness);
+        this.log("Current brightness: " + this.brightness);
         callback(null, this.brightness);
     },
 
@@ -144,6 +215,9 @@ OscAccessory.prototype = {
     },
 
     getServices: function () {
+        var self = this;
+
+        this.services = [];
 
         // you can OPTIONALLY create an information service if you wish to override
         // the default values for things like serial number, model, etc.
@@ -154,19 +228,46 @@ OscAccessory.prototype = {
 			.setCharacteristic(Characteristic.Model, "OSC Model")
 			.setCharacteristic(Characteristic.SerialNumber, "OSC Serial Number");
 
-        if (this.service == "Switch") {
-            this._service = new Service.Switch(this.name);
+        //this.services.push(informationService);
 
-            this._service
-				.getCharacteristic(Characteristic.On)
-				.on('set', this.setPowerState.bind(this));
+        switch (this.accessoryType) {
+            case "Switch":
+                this._service = new Service.Switch(this.name);
 
-            this._service
-                .addCharacteristic(Characteristic.Brightness)
-                .on('set', this.setBrightness.bind(this))
-                .on('get', this.getBrightness.bind(this));
+                this._service
+				    .getCharacteristic(Characteristic.On)
+				    .on('set', this.setPowerState.bind(this));
 
-            return [this._service];
+                this._service
+                    .addCharacteristic(Characteristic.Brightness)
+                    .on('set', this.setBrightness.bind(this))
+                    .on('get', this.getBrightness.bind(this));
+
+                this.services.push(this._service);
+                break;
+            case "Multiswitch":
+                // Multiple switches in a radio button style
+                // Turning one on will turn the others off. Switches are sent as args to the specified address in the order
+                // they appear in the config file. If switches = ["alpha", "beta"], selecting alpha will send "/osc 1.0 0"; selecting
+                // beta will sent "/osc 0 1.0"
+
+                this.log("Adding multiswitch to " + this.name);
+                for (var i = 0; i < this.switches.length; i++) {
+                    var switchName = this.switches[i];
+                    var service = new Service.Switch(switchName, switchName);
+
+                    // Bind a copy of the setPowerState function that sets "this" to the accessory and the first parameter
+                    // to the particular service that it is being called for. 
+                    var mySetPowerState = this.setPowerState.bind(this, service);
+                    service.getCharacteristic(Characteristic.On).on('set', mySetPowerState);
+
+                    this.services.push(service);
+                    this.log("Added switch " + switchName + " to " + this.name)
+                }
+                break;
+            default:
+                throw new Error("Unknown homebridge-osc accessory type in getServices");
         }
+        return this.services;
     }
 };
